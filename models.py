c0725b78d0dfb1101c5fb130f88c272ddde7a48f
Juan Pablo Arbelaez Gomez <juanpablo-arbelaez@hotmail.com>
2020-12-04 18:30:47 +0700

    Update models.py

--- models.py
+++ models.py
@@ -60,33 +60,83 @@ class ContractingBlock(nn.Module):
         return x      
 
 
+# class ExpandingBlock(nn.Module):
+#     """ExpandingBlock Class
+#         Performs an upsampling, a convolution, a concat of its two inputs, followed by two more convolutions with optional droput
+#     Args:
+#         input_channels (int): the number of input channels to expect from a given input
+#     """
+#     def __init__(self, input_channels, use_dropout=False, use_bn=True):
+#         super().__init__()
+#         self.upsample = nn.Upsample(scale_factor=2, mode="bilinear", align_corners=True)
+#         self.conv1 = nn.Conv2d(input_channels, input_channels // 2, kernel_size=2)
+#         self.conv2 = nn.Conv2d(input_channels, input_channels // 2, kernel_size=3, padding=1)
+#         self.conv3 = nn.Conv2d(input_channels // 2, input_channels // 2, kernel_size=2, padding=1)
+#         self.activation = nn.ReLU()
+#         self.use_bn = use_bn
+#         if self.use_bn:
+#             self.batchnorm = nn.BatchNorm2d(input_channels * 2)
+#         self.use_dropout = use_dropout
+#         if self.use_dropout:
+#             self.dropout = nn.Dropout()
+
+#     def forward(self, x, skip_con_x):
+#         """Method for completing a forward pass of the ExpandingBlock:
+
+#         Args:
+#             x (tensor): iamge tensor of shape (batch_size, num_channels, height, width)
+#             skip_con_x (tensor): image tensor from the contracting path (from the ooposing block of x)
+#         """
+#         x = self.upsample(x)
+#         x = self.conv1(x)
+#         skip_con_x = crop(skip_con_x, x.shape)
+#         x = torch.cat([x, skip_con_x], axis=1)
+#         x = self.conv2(x)
+#         if self.use_bn:
+#             x = self.batchnorm(x)
+#         if self.use_dropout:
+#             x = self.dropout(x)
+#         x = self.activation(x)
+#         x = self.conv3(x)
+#         if self.use_bn:
+#             x = self.batchnorm(x)
+#         if self.use_dropout:
+#             x = self.dropout(x)
+#         x = self.activation(x)
+#         return x
+
+
 class ExpandingBlock(nn.Module):
-    """ExpandingBlock Class
-        Performs an upsampling, a convolution, a concat of its two inputs, followed by two more convolutions with optional droput
-    Args:
-        input_channels (int): the number of input channels to expect from a given input
-    """
+    '''
+    ExpandingBlock Class:
+    Performs an upsampling, a convolution, a concatenation of its two inputs,
+    followed by two more convolutions with optional dropout
+    Values:
+        input_channels: the number of channels to expect from a given input
+    '''
     def __init__(self, input_channels, use_dropout=False, use_bn=True):
-        super().__init__()
-        self.upsample = nn.Upsample(scale_factor=2, mode="bilinear", align_corners=True)
+        super(ExpandingBlock, self).__init__()
+        self.upsample = nn.Upsample(scale_factor=2, mode='bilinear', align_corners=True)
         self.conv1 = nn.Conv2d(input_channels, input_channels // 2, kernel_size=2)
         self.conv2 = nn.Conv2d(input_channels, input_channels // 2, kernel_size=3, padding=1)
         self.conv3 = nn.Conv2d(input_channels // 2, input_channels // 2, kernel_size=2, padding=1)
-        self.activation = nn.ReLU()
+        if use_bn:
+            self.batchnorm = nn.BatchNorm2d(input_channels // 2)
         self.use_bn = use_bn
-        if self.use_bn:
-            self.batchnorm = nn.BatchNorm2d(input_channels * 2)
-        self.use_dropout = use_dropout
-        if self.use_dropout:
+        self.activation = nn.ReLU()
+        if use_dropout:
             self.dropout = nn.Dropout()
+        self.use_dropout = use_dropout
 
     def forward(self, x, skip_con_x):
-        """Method for completing a forward pass of the ExpandingBlock:
-
-        Args:
-            x (tensor): iamge tensor of shape (batch_size, num_channels, height, width)
-            skip_con_x (tensor): image tensor from the contracting path (from the ooposing block of x)
-        """
+        '''
+        Function for completing a forward pass of ExpandingBlock: 
+        Given an image tensor, completes an expanding block and returns the transformed tensor.
+        Parameters:
+            x: image tensor of shape (batch size, channels, height, width)
+            skip_con_x: the image tensor from the contracting path (from the opposing block of x)
+                    for the skip connection
+        '''
         x = self.upsample(x)
         x = self.conv1(x)
         skip_con_x = crop(skip_con_x, x.shape)
